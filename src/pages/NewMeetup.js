16709216bbf3159da022a8c6e3b06e2030bb6ddf
Nicolas Corbalan <outreef@hotmail.com>
2021-05-16 20:42:46 +0200

    adding new meetup form

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import NewMeetupForm from '../components/meetups/NewMeetupForm'
 
 function NewMeetupPage() {
     return (
-        <div>
-            New Meetup Page
-        </div>
+        <section>
+            <h1>Add New Meetup</h1>
+            <NewMeetupForm />
+        </section>
     )
 }
 
