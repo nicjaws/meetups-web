d15de92d55935cd13888d0292f151c900bf17a1d
Nicolas Corbalan <outreef@hotmail.com>
2021-05-19 22:35:27 +0200

    connecting firebase database

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -2,10 +2,21 @@ import React from 'react'
 import NewMeetupForm from '../components/meetups/NewMeetupForm'
 
 function NewMeetupPage() {
+  function addMeetupHandler(meetupData) {
+    fetch('https://meetups-93434-default-rtdb.firebaseio.com/meetups.json',
+    {
+      method: 'POST',
+      body: JSON.stringify(meetupData),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }
+    );
+  }
     return (
         <section>
             <h1>Add New Meetup</h1>
-            <NewMeetupForm />
+            <NewMeetupForm onAddMeetup={addMeetupHandler} />
         </section>
     )
 }
